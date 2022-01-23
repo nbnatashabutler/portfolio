bddc10944c3532fc07f97b9ab95dfde8b66d8b9d
Natasha Butler <nbnatashabutler@gmail.com>
2022-01-23 10:22:46 -0500

    data to png jpeg svg

--- canvas.js
+++ canvas.js
@@ -17,3 +17,50 @@ var signaturePad = new SignaturePad(canvas,
 backgroundColor: 'rgb(255, 255, 255)'
 });
 
+document.getElementById('save-png').addEventListener
+('click'), function ()
+{
+   if (signaturePad.isEmpty())
+{
+
+   return alert
+("Would you like to create something?");
+}
+
+var data = signaturePad.toDataURL('image/png');
+
+console.log(data);
+window.open(data);});
+
+document.getElementById('save-png').addEventListener
+('click'), function ()
+{
+   if (signaturePad.isEmpty())
+{
+
+   return alert
+("Would you like to create something?");
+}
+
+var data = signaturePad.toDataURL('image/jpeg');
+
+console.log(data);
+window.open(data);});
+
+document.getElementById('save-png').addEventListener
+('click'), function ()
+{
+   if (signaturePad.isEmpty())
+{
+
+   return alert
+("Would you like to create something?");
+}
+
+var data = signaturePad.toDataURL('image/svg+xml');
+
+console.log(data);
+console.log(atob(data.split(',')[1]));
+window.open(data);});
+
+
